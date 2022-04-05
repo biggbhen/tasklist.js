2fd7e5ed01e8feee046c9602bfe3a44f0ef7be1e
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-04-05 20:13:18 +0100

    finished the filter property

--- app.js
+++ app.js
@@ -1,48 +1,60 @@
-const form =  document.querySelector('#task-form')
+const form = document.querySelector('#task-form');
 const taskList = document.querySelector('.collection');
 const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
-
 loadAllEventListeners();
-function loadAllEventListeners(){
- form.addEventListener('submit', addTask);
- taskList.addEventListener('click', removeTask)
- clearBtn.addEventListener('click', clearTask)
+function loadAllEventListeners() {
+  form.addEventListener('submit', addTask);
+  taskList.addEventListener('click', removeTask);
+  clearBtn.addEventListener('click', clearTask);
+  filter.addEventListener('keyup', filterTask);
 }
 
-function addTask(e){
- const li = document.createElement('li')
- li.className = 'collection-item'
- const textResult = document.createTextNode(taskInput.value);
-li.appendChild(textResult);
+function addTask(e) {
+  const li = document.createElement('li');
+  li.className = 'collection-item';
+  const textResult = document.createTextNode(taskInput.value);
+  li.appendChild(textResult);
 
-const link = document.createElement('a');
-link.className = 'delete-item secondary-content'
-link.innerHTML= '<i class="fa fa-remove"></i>'
-li.appendChild(link);
-taskList.appendChild(li)
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  li.appendChild(link);
+  taskList.appendChild(li);
 
-taskInput.value=''
-// console.log(li);
- e.preventDefault()
+  taskInput.value = '';
+  // console.log(li);
+  e.preventDefault();
 }
 
 // remove task
-function removeTask(e){
- // console.log(e.target.parentElement.classList);
- if(e.target.parentElement.classList.contains('delete-item')){
-e.target.parentElement.parentElement.remove();
- }
-//  console.log(e);
+function removeTask(e) {
+  // console.log(e.target.parentElement.classList);
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    e.target.parentElement.parentElement.remove();
+  }
+  //  console.log(e);
 }
 
-function clearTask(e){
-// console.log(e.target);
-while(taskList.firstChild){
-taskList.removeChild(taskList.firstChild);
+function clearTask(e) {
+  // console.log(e.target);
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+  e.preventDefault();
 }
 
-e.preventDefault()
+function filterTask(e) {
+  const text = e.target.value.toLowerCase();
+  const lis = document.querySelectorAll('.collection-item');
+  lis.forEach(function (x) {
+    const item = x.firstChild.textContent.toLowerCase();
+    if (item.indexOf(text) != -1) {
+      x.style.display = 'block';
+    } else {
+      x.style.display = 'none';
+    }
+  });
 }
