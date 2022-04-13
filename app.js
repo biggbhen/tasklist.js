17ea427d541724cd3b3857a8adf606a781a746ab
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-04-13 23:43:09 +0100

    almost done

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function loadAllEventListeners() {
   taskList.addEventListener('click', removeTask);
   clearBtn.addEventListener('click', clearTask);
   filter.addEventListener('keyup', filterTask);
+  document.addEventListener('DOMContentLoaded', getTasks)
 }
 
 function addTask(e) {
@@ -24,18 +25,76 @@ function addTask(e) {
   li.appendChild(link);
   taskList.appendChild(li);
 
+  
+  addTaskToLocalStorage(taskInput.value)
   taskInput.value = '';
   // console.log(li);
   e.preventDefault();
 }
+
+// get Tasks from localStorage
+
+function getTasks(task) {
+   let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+   }else{
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+  tasks.push(task)
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+
+  tasks.forEach(function(task){
+    const li = document.createElement('li');
+  li.className = 'collection-item';
+  const textResult = document.createTextNode(task);
+  li.appendChild(textResult);
+
+  const link = document.createElement('a');
+  link.className = 'delete-item secondary-content';
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  li.appendChild(link);
+  taskList.appendChild(li);
+  })
+}
+
+// add task to local storage
+function addTaskToLocalStorage(task){
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+   }else{
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+  tasks.push(task)
+  localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+
 
 // remove task
 function removeTask(e) {
   // console.log(e.target.parentElement.classList);
   if (e.target.parentElement.classList.contains('delete-item')) {
     e.target.parentElement.parentElement.remove();
+
+    removeTaskFromLocalStorage(e.target.parentElement.parentElement)
   }
-  //  console.log(e);
+  //  console.log(e.target);
+}
+function removeTaskFromLocalStorage (taskItem) {
+  let tasks;
+  if (localStorage.getItem('tasks') === null) {
+    tasks = [];
+   }else{
+    tasks = JSON.parse(localStorage.getItem('tasks'))
+  }
+  tasks.forEach(function(task, index) {
+    if(taskItem.textContent === task){
+    tasks.splice(index, 1)
+    }
+  })
+  localStorage.setItem('tasks', JSON.stringify(tasks))
 }
 
 function clearTask(e) {
